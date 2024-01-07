04b4c88d7fb56b88e0c8097abe8372163330bf4d
Philipp Hess <philipp-hess@hotmail.de>
2024-01-07 16:05:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function checkPalindrome() {
+  const userInput = document
+    .getElementById("text-input")
+    .value.toLowerCase()
+    .replace(/[^a-z0-9]/g, "");
+
+  if (userInput === "") {
+    alert("Please input a value");
+    return;
+  }
+
+  const reversedInput = userInput.split("").reverse().join("");
+
+  if (userInput === reversedInput) {
+    document.getElementById("result").innerText =
+      document.getElementById("text-input").value + " is a palindrome";
+  } else {
+    document.getElementById("result").innerText =
+      document.getElementById("text-input").value + " is not a palindrome";
+  }
+}
